e18e3fc91c815933691807036f71a4442870a4be
Dan Smith <dsmith@danplanet.com>
2015-04-28 16:41:28 -0700

    Add some missing requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='wsevents',
       version='1.0',
       packages=['wsevents'],
       scripts=['event_service', 'event_handler'],
+      install_requires=['asyncio', 'aiohttp', 'puredaemon', 'setproctitle',
+                        'stevedore'],
   )
